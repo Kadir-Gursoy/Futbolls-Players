fd761aaa208fab28e4126e5373fd3b1e6aabc08c
Abdulkadir Gursoy <131687998+Kadir-Gursoy@users.noreply.github.com>
2023-11-22 23:22:38 +0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.scss";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Türkiye'ye Gelmiş En İyi Yabancı Futbolcular</h1>
+      <h1>En İyi Yabancı Futbolcular</h1>
       
     </div>
   );
